61953c7674262e2edb860d1195d649861accf82f
gitputri <pputthyuu78@gmail.com>
2024-02-21 16:13:08 -0800

    commit pertama

---
+++ register.php
@@ -0,0 +1,107 @@
+<?php 
+   include "koneksi.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Silahkan Register</title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-7">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Register!</h3></div>
+                                    <div class="card-body">
+                                    <?php 
+                                        if(isset ($_SESSION['register.php'])){
+                                        $username= $_POST(["username"]);
+                                        $password= MD5($_POST["password"]);
+                                        $email= $_POST(["email"]);
+                                        $nama_lengkap= $_POST(["nama_lengkap"]);
+                                        $alamat= $_POST(["alamat"]);
+
+                                        $data = mysqli_query($koneksi, "SELECT*FROM user where 'username' = $username,'password' = $password, 'email' = $email, 'nama_lengkap' = $nama_lengkap and 'alamat' = $alamat" );
+                                        $cek = mysqli_num_rows ($data);
+                                        }
+ 
+                                        ?>        
+                                    
+                                    <form method="post">
+                                        <form>
+                                            <div class="row mb-3">
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" id="inputUsername" type="text" placeholder="Enter your username" />
+                                                        <label for="inputUsername">Username</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-floating">
+                                                        <input class="form-control" id="inputPassword" type="password" placeholder=" a pasCreatesword" />
+                                                        <label for="inputPassword">Password</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
+                                                <label for="inputEmail">Email address</label>
+                                            </div>
+                                            <div class="row mb-3">
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" id="inputNamalengkap" type="namalengkap" placeholder="Enter your nama lengkap" />
+                                                        <label for="inputNamalengkap">Nama Lengkap</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" id="inputAlamat" type="Alamat" placeholder="Alamat" />
+                                                        <label for="Alamat">Alamat</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="mt-4 mb-0">
+                                                <div class="d-grid"><a class="btn btn-primary btn-block" href="login.php">Submit</a></div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="card-footer text-center py-3">
+                                        <div class="small"><a href="login.php">Have an account? Go to login</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Perpustakaan Digital2024</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
